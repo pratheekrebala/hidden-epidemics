858ca06e75472c433c150eecf392f872d1e797d5
Pratheek Rebala <pratheekrebala@gmail.com>
2018-08-01 17:29:43 -0400

    fix categorization
    Signed-off-by: Pratheek Rebala <pratheekrebala@gmail.com>

--- dist/process.sh
+++ dist/process.sh
@@ -22,14 +22,17 @@ do
     gdalwarp -overwrite -co COMPRESS=LZW -dstnodata -9999 -cutline northeast.geojson -crop_to_cutline -dstalpha -ts 19515 8626 -r bilinear $fl $clipped
 
     echo "Re-classifying $clipped"
-    calculation="0*(A<800)+1700*(A>1600)+((A//80)*80)*((A>=800)*(A<=1600))"
+    # 800 => A < 880
+    # 1700 => A > 1680
+    # (A//80)*80 => A >= 880 & A <= 1680
+    calculation="800*(A<880)+1700*(A>1680)+((A//80)*80)*((A>=880)*(A<=1680))"
     gdal_calc.py --quiet --overwrite --co COMPRESS=LZW -A $clipped --outfile=$reclassified --calc=$calculation --NoDataValue=-9999
 
     echo "Applying color-relief on $reclassified"
     gdaldem color-relief -alpha -compute_edges -co COMPRESS=LZW $reclassified rgba.txt $colorized
 
     echo "Converting $colorized to $mbtiles."
-    gdal2mbtiles --name $file --min-resolution=1 --max-resolution=8 --format="jpg" $colorized $mbtiles
+    gdal2mbtiles --name $file --min-resolution=1 --max-resolution=7 --format="jpg" $colorized $mbtiles
 
     echo "Exporting $mbtiles"
     rm -rf $tiles
