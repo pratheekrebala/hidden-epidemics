e5be3b97f5ed65f20f223d38f887ad0a7f5c83fd
Pratheek Rebala <pratheekrebala@gmail.com>
2018-07-31 22:20:27 -0400

    Check in updates
    Signed-off-by: Pratheek Rebala <pratheekrebala@gmail.com>

--- dist/process.sh
+++ dist/process.sh
@@ -2,12 +2,20 @@ mkdir -p clipped
 mkdir -p resampled
 mkdir -p reclassified
 mkdir -p colorized
+mkdir -p mbtiles
+mkdir -p tiles
 
 for fl in `ls -d $PWD/source/*.tiff`
 do
-    clipped="clipped/`basename $fl`"
-    reclassified="reclassified/`basename $fl`"
-    colorized="colorized/`basename $fl`"
+    fileExt=${fl##*/}
+    file=${fileExt%.*}
+    echo $file
+
+    clipped="clipped/${file}.tiff"
+    reclassified="reclassified/${file}.tiff"
+    colorized="colorized/${file}.tiff"
+    mbtiles="mbtiles/${file}.mbtiles"
+    tiles="tiles/${file}"
 
     echo "Clipping and Upscaling $fl"
     # Clip to Northeast & Resample (Bilinear with 0.003 Cell Size; Aligned to Extent)
@@ -15,8 +23,21 @@ do
 
     echo "Re-classifying $clipped"
     calculation="0*(A<800)+1700*(A>1600)+((A//80)*80)*((A>=800)*(A<=1600))"
-    gdal_calc.py --overwrite --co COMPRESS=LZW -A $clipped --outfile=$reclassified --calc=$calculation --NoDataValue=-9999
+    gdal_calc.py --quiet --overwrite --co COMPRESS=LZW -A $clipped --outfile=$reclassified --calc=$calculation --NoDataValue=-9999
 
     echo "Applying color-relief on $reclassified"
-    gdaldem color-relief -alpha -compute_edges -co COMPRESS=LZW $reclassified rgba.txt $colorized
+    gdaldem color-relief -alpha -compute_edges -co COMPRESS=LZW $reclassified rgba1.txt $colorized
+
+    echo "Converting $colorized to $mbtiles."
+    gdal2mbtiles --name $file --min-resolution=1 --max-resolution=8 --format="jpg" $colorized $mbtiles
+    #gdalwarp -t_srs EPSG:3857 -r near "${colorized}" "${colorized}_projected"
+    #gdal_translate -of mbtiles "${colorized}_projected" $mbtiles
+    #gdaladdo -r nearest $mbtiles
+
+    #rm "${colorized}_projected"
+
+    echo "Exporting $mbtiles"
+    rm -rf $tiles
+    mb-util --silent --image_format=png --do_compression $mbtiles $tiles
+
 done;
